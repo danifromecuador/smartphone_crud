d689c573b118bb72f861e9ad9f49070ed6aa2b3e
dani <danifromecuador@gmail.com>
2024-07-16 13:21:59 -0500

    Implement C for CRUD on Phone resource

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -2,4 +2,21 @@ class PhonesController < ApplicationController
   def index
     @phones = Phone.all
   end
+
+  def new
+    @phone = Phone.new
+  end
+
+  def create
+    @phone = Phone.new(phone_params)
+    if @phone.save
+      redirect_to "/"
+    end
+  end
+
+  private
+
+  def phone_params
+    params.require(:phone).permit(:pbrand, :pmodel)
+  end
 end
